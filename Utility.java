7b5c7aef7ff5e4730f3692bdc42032fe87ce3dbc
aymanaliuwod <saasa735@uowmail.edu.au>
2025-06-21 17:55:59 +0400

    Create Utility.java

---
+++ Utility.java
@@ -0,0 +1,9 @@
+package ECTE331_Project_Q3;
+
+public class Utility {
+
+    public static int calculateSum(int n) {
+        // As per the formula given: n * (n + 1) / 2
+        return n * (n + 1) / 2;
+    }
+}
